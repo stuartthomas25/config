c69dfccfb19cf81dbcf0065d61954c9158aa6840
Stuart Thomas <snthomas@umd.edu>
2022-08-30 18:41:50 -0400

    initial commit

---
+++ doom/custom.el
@@ -0,0 +1,60 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#fbf1c7" "#9d0006" "#79740e" "#b57614" "#076678" "#b16286" "#427b58" "#282828"])
+ '(custom-safe-themes
+   '("4f1d2476c290eaa5d9ab9d13b60f2c0f1c8fa7703596fa91b235db7f99a9441b" "5784d048e5a985627520beb8a101561b502a191b52fa401139f4dd20acb07607" default))
+ '(exwm-floating-border-color "#d3c5a0")
+ '(fci-rule-color "#504945")
+ '(highlight-tail-colors ((("#eee4b4" "#f3f3c1") . 0) (("#e8e5bb" "#eff3cf") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#f0f0f0" "#a89984"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#f0f0f0" "#79740e"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#f0f0f0" "#928374"))
+ '(objed-cursor-color "#9d0006")
+ '(org-agenda-files
+   '("/home/stuart/Projects/open-system-dissipation/personal-notes.org" "/home/stuart/org/todo.org" "/home/stuart/org/jay-notes.org"))
+ '(package-selected-packages '(zotero ein))
+ '(pdf-misc-print-program-executable "/opt/homebrew/bin/gpr")
+ '(rustic-ansi-faces
+   ["#fbf1c7" "#9d0006" "#79740e" "#b57614" "#076678" "#b16286" "#427b58" "#282828"])
+ '(safe-local-variable-values
+   '((eval conda-env-activate-path "~/.conda/envs/rscience")
+     (eval set-input-method 'TeX)))
+ '(vc-annotate-background "#fbf1c7")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#79740e")
+    (cons 40 "#8d7410")
+    (cons 60 "#a17512")
+    (cons 80 "#b57614")
+    (cons 100 "#b3620e")
+    (cons 120 "#b14e08")
+    (cons 140 "#af3a03")
+    (cons 160 "#af472e")
+    (cons 180 "#b0545a")
+    (cons 200 "#b16286")
+    (cons 220 "#aa415b")
+    (cons 240 "#a32030")
+    (cons 260 "#9d0006")
+    (cons 280 "#9a2021")
+    (cons 300 "#97413c")
+    (cons 320 "#946258")
+    (cons 340 "#504945")
+    (cons 360 "#504945")))
+ '(vc-annotate-very-old-color nil)
+ '(warning-suppress-types
+   '((doom-first-file-hook)
+     (doom-first-file-hook)
+     (doom-first-file-hook)
+     (doom-first-file-hook)
+     (:warning))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'customize-variable 'disabled nil)

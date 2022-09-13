a5928df160a786d8950f89db7af3054c2539ee38
Stuart Thomas <snthomas@umd.edu>
2022-09-13 15:03:44 -0400

    add vimrc

--- init.sh
+++ init.sh
@@ -2,3 +2,6 @@
 git submodule update --init --recursive
 find $HOME/.config -name "*.org" -exec emacs -q --batch --eval "(require 'org)" --eval '(org-babel-tangle-file "{}")' \;
 
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall > /dev/null 2>&1

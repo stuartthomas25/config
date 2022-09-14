c4ada7c465bad90f88fabfb7f5d5d934e2efef98
Stuart Thomas <snthomas@umd.edu>
2022-09-14 10:50:15 -0400

    only install vim-plugged if missing

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 git submodule update --init --recursive
 find $HOME/.config -name "*.org" -exec emacs -q --batch --eval "(require 'org)" --eval '(org-babel-tangle-file "{}")' \;
 
-
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if ! [[ -e ~/.vim/autoload/plug.vim ]]; then
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 vim +PlugInstall +qall > /dev/null 2>&1

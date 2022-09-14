2b0f84203269520470f43213eff7da63b034cf40
Stuart Thomas <snthomas@umd.edu>
2022-09-14 11:30:36 -0400

    improve tangling procedure

--- init.sh
+++ init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 git submodule update --init --recursive
-find $HOME/.config -name "*.org" -exec emacs -q --batch --eval "(require 'org)" --eval '(org-babel-tangle-file "{}")' \;
+# find $CONFIG -name "*.org" -exec emacs -q --batch --eval "(require 'org)" --eval '(org-babel-tangle-file "{}")' \;
+
+emacscommands=$(find $CONFIG -name "*.org" | awk '{print "(org-babel-tangle-file \"" $0 "\")"}' | paste -s -d' ');
+emacs -q --batch --eval "(require 'org)" --eval "(progn $emacscommands)"
 
 if ! [[ -e ~/.vim/autoload/plug.vim ]]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
-vim +PlugInstall +qall > /dev/null 2>&1
+vim +PlugInstall +qall

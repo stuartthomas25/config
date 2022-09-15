77f18985643ce00f2d54023c59e62b3e8b523c51
Stuart Nathan Thomas <stuartthomas25@gmail.com>
2022-09-15 19:09:33 -0400

    add source zshrc

--- init.sh
+++ init.sh
@@ -9,3 +9,5 @@ if ! [[ -e ~/.vim/autoload/plug.vim ]]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 vim +PlugInstall +qall
+
+source ~/.zshrc

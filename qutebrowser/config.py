fa3c90d52681168a5ef921541cb9ff9640e07d35
Stuart Thomas <snthomas@umd.edu>
2023-06-24 16:54:53 -0400

    clean up repo

--- qutebrowser/config.py
+++
@@ -1,6 +0,0 @@
-from os.path import exists, expanduser
-config.load_autoconfig()
-config.source('qutebrowser-themes/themes/gruvbox.py')
-LIGHT = exists(expanduser("~/.light"))
-c.colors.webpage.preferred_color_scheme = 'light' if LIGHT else 'dark'
-
